c250843a5a71703ac0b29536e34daa653920fd67
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-24 13:31:34 +0200

    step 3

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 let ul = document.getElementsByTagName('ul');
 let input = document.getElementsByTagName('input');
 let button = document.getElementsByTagName('button');
+function buttonClick() {
+    
+}
