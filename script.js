1bea7ad1bc6e9b91e0d610b21b77dbde85d40356
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-01-24 13:38:51 +0200

    step 6

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ let button = document.getElementsByTagName('button');
 button.onclick = function() {
     let myInput = input.value;
     input.value = '';
+
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    let liButton = document.createElement('button');
+    
 }
